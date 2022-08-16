d2cfb95f5c0dad296b0ee27d97225fd9686005ed
ItzMari0 <msajr@outlook.com>
2022-08-16 18:56:01 -0300

    Requisito 4 Resolvido

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,18 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import NotFound from '../pages/NotFound';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente <NotFound />', () => {
+  it('contém um heading "h2" com o texto "Page Request Not Found"', () => {
+    renderWithRouter(<NotFound />);
+    const notFound = screen.getByText('Page requested not found');
+    expect(notFound).toBeInTheDocument();
+  });
+
+  it('a página mostra a imagem "https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif"', () => {
+    renderWithRouter(<NotFound />);
+    const image = screen.getByAltText(/Pikachu crying/i);
+    expect(image.src).toContain('https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
