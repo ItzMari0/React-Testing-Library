ccd02ae9b51686595195733da2f17c573d4839d0
ItzMari0 <msajr@outlook.com>
2022-08-16 18:43:46 -0300

    Requisito 3 Resolvido

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,12 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import FavoritePokemons from '../pages/FavoritePokemons';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testa o componente <FavoritePokemons />', () => {
+  it('é exibida a mensagem "No favorite pokemon found", caso não haja favoritos', () => {
+    renderWithRouter(<FavoritePokemons />);
+    const noFavorite = screen.getByText('No favorite pokemon found');
+    expect(noFavorite).toBeInTheDocument();
+  });
+});
