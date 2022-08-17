04bd8b3d82aafd284dded9392cc85632707dcfff
ItzMari0 <msajr@outlook.com>
2022-08-16 21:23:54 -0300

    Requisito 5 Resolvido

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -30,9 +30,28 @@ describe('Testa o componente <Pokedex />', () => {
     expect(secondPokemon).toBeInTheDocument();
   });
 
-  it('é mostrado apenas um pokémon por vez', () => {
+  it('se a Pokédex tem os botões de filtro', () => {
     renderWithRouter(<App />);
-    const pokemon = screen.getByTestId('pokemon-name');
-    expect(pokemon).toHaveLength(1);
+    const pokemonsTypeBtn = 7;
+    const filterBtn = screen.getAllByTestId('pokemon-type-button');
+    expect(filterBtn).toHaveLength(pokemonsTypeBtn);
+  });
+
+  it('o texto do botão deve corresponder ao tipo', () => {
+    renderWithRouter(<App />);
+    const fireBtn = screen.getByRole('button', { name: 'Fire' });
+
+    userEvent.click(fireBtn);
+    const firePokemons = screen.getAllByText('Fire');
+    expect(firePokemons).toHaveLength(2);
+  });
+
+  it('contém um botão para resetar o filtro', () => {
+    renderWithRouter(<App />);
+    const allBtn = screen.getByRole('button', { name: 'All' });
+
+    userEvent.click(allBtn);
+    const firstPokemon = screen.getByText('Pikachu');
+    expect(firstPokemon).toBeInTheDocument();
   });
 });
